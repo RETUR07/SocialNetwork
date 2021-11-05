e1daaf260a4cb3324e3ca8e89bda146335da33e9
RETUR <nikitosix2002@gmail.com>
2021-11-05 18:11:13 +0300

    Added post model

---
+++ Entities/Models/Blob.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public enum FileTypes
+    {
+        Image,
+        TextDocument,
+        Video
+    }
+    public class Blob
+    {
+        public int Id { get; set; }
+        public FileTypes FileType { get; set; }
+        public byte[] Buffer { get; set; }
+    }
+}
