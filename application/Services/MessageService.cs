7120911b0f11aeef148236b4eeca937f60602eb3
retur <nikitosix2002@gmail.com>
2021-11-24 19:19:14 +0300

    tesst queue

---
+++ application/Services/MessageService.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using SocialNetwork.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Application.Services
+{
+    public class MessageService : IMessageService
+    {
+        ConnectionFactory _factory;
+        IConnection _conn;
+        IModel _channel;
+        public MessageService()
+        {
+            Console.WriteLine("about to connect to rabbit");
+
+            _factory = new ConnectionFactory() { HostName = "rabbitmq", Port = 5672 };
+            _factory.UserName = "guest";
+            _factory.Password = "guest";
+            _conn = _factory.CreateConnection();
+            _channel = _conn.CreateModel();
+            _channel.QueueDeclare(queue: "hello",
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+        }
+        public bool Enqueue(string messageString)
+        {
+            var body = Encoding.UTF8.GetBytes("server processed " + messageString);
+            _channel.BasicPublish(exchange: "",
+                                routingKey: "hello",
+                                basicProperties: null,
+                                body: body);
+            Console.WriteLine(" [x] Published {0} to RabbitMQ", messageString);
+            return true;
+        }
+    }
+}
