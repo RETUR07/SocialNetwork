a31d930b61bd1ac65181af58074cf328adf3f702
RETUR07 <75529203+RETUR07@users.noreply.github.com>
2021-12-29 12:11:03 +0300

    added refreshToken to response

--- application/Security/DTO/AuthenticateResponse.cs
+++ application/Security/DTO/AuthenticateResponse.cs
@@ -15,8 +15,6 @@ namespace SocialNetwork.Security.DTO
         public string LastName { get; set; }
         public string Username { get; set; }
         public string JwtToken { get; set; }
-
-        [JsonIgnore]
         public string RefreshToken { get; set; }
 
         public AuthenticateResponse(User user, string jwtToken, string refreshToken)
