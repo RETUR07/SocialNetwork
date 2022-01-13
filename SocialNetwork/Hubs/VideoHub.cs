fbc1b2e60ea5bee097791846b84bb50768466521
RETUR07 <75529203+RETUR07@users.noreply.github.com>
2022-01-13 14:50:20 +0300

    DownloadStream

--- SocialNetwork/Hubs/VideoHub.cs
+++ SocialNetwork/Hubs/VideoHub.cs
@@ -8,15 +8,22 @@ namespace SocialNetwork.Hubs
 {
     public class VideoHub :BaseHub
     {
+        private Channel<string> channel = Channel.CreateUnbounded<string>();
+
         public async Task UploadStream(ChannelReader<string> stream)
         {
             while (await stream.WaitToReadAsync())
             {
                 while (stream.TryRead(out var item))
                 {
-                    await Clients.All.SendAsync("GetStreamPart", item);
+                    channel.Writer.TryWrite(item);
                 }
             }
+        }
+
+        public ChannelReader<string> DownloadStream()
+        {
+            return channel.Reader;
         }
     }
 }
